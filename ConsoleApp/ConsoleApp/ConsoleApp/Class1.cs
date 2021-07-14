dd9ea9b4a36a406474967ecbbc762261951cfdbe
谈涛 <tan_tao@foxmail.com>
2021-07-14 13:46:26 +0800

    add Class1

---
+++ ConsoleApp/ConsoleApp/ConsoleApp/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Class1
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
